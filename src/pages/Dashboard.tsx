1ad2002b8db10005f0c7ae152ce3b7e2a552995e
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2024-04-03 23:44:53 -0230

    Updated Dashboard to display correctly

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router";
 import Sidebar from "../components/Sidebar.tsx";
+import Footer from "../components/Footer.tsx";
 
 const Dashboard = () => {
   return (
     <div>
-      <Sidebar />
-      <Outlet />
+      <div className='flex flex-row'>
+        <div>
+          <Sidebar />
+        </div>
+        <div className='flex-1 h-screen ml-5'>
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
