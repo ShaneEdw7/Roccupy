7c6f3e37e2853b0b1e4fa371fb354d357c829693
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2024-04-18 20:39:53 -0230

    Create index for icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,17 @@
+import PropertyIcon from "./PropertyIcon";
+import ApplicationsIcon from "./ApplicationsIcon";
+import AccountsIcon from "./AccountsIcon";
+import MaintenanceIcon from "./MaintenanceIcon";
+import CommunicationsIcon from "./CommunicationsIcon";
+import FilesIcon from "./FilesIcon";
+import ReportsIcon from "./ReportsIcon";
+
+export {
+  PropertyIcon,
+  ApplicationsIcon,
+  AccountsIcon,
+  MaintenanceIcon,
+  CommunicationsIcon,
+  FilesIcon,
+  ReportsIcon,
+};
