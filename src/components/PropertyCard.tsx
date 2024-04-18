63e894e9cd9a445998b312639a61756dd4e1aa9d
ShaneEdw7 <130687464+ShaneEdw7@users.noreply.github.com>
2024-04-18 20:36:32 -0230

    Create Property Card Template

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -3,15 +3,22 @@ import RowHouses from "../assets/images/jellybeanrowtest.jpg";
 const PropertyCard = () => {
   return (
     <div>
-      <div className='card w-96 bg-base-100 shadow-xl'>
-        <figure className='px-10 pt-10'>
+      <div className='card lg:w-96 bg-base-100 shadow-xl'>
+        <figure className='px-4 pt-4'>
           <img src={RowHouses} alt='Property Image' className='rounded-xl' />
         </figure>
-        <div className='card-body items-end text-center'>
-          <h2 className='card-title'>Property</h2>
-          <p></p>
+        <div className='card-body text-center grid grid-cols-2 items-end justify-items-center'>
+          <div>
+            <h2 className='card-title'>123 Evergreen Terrace</h2>
+          </div>
+          <div>
+            <p>Property Info 1</p>
+            <p>Property Info 2</p>
+            <p>Property Info 3</p>
+          </div>
+          <div>Additional Info</div>
           <div className='card-actions'>
-            <button className='btn btn-primary'>View</button>
+            <button className='btn btn-primary w-24'>View</button>
           </div>
         </div>
       </div>
